279b6a87c925746902f111ac590f1ab8d892a09c
Daniel Lousa <dlousa92@gmail.com>
2018-09-04 14:10:52 -0400

    when pressing create, add a new item to the to do list

--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -16,12 +16,25 @@ class TodoForm extends Component {
     })
   }
 
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      item: '',
+      itemList: [...this.state.itemList, this.state.item]
+    })
+  }
+
   render () {
     return (
       <div>
         <h1>Hello, Daniel</h1>
 
-        <input value={this.state.item} onChange={this.handleInput} />
+
+        <form onSubmit={this.onSubmit}>
+      
+          <input value={this.state.item} onChange={this.handleInput} />
+          <button>Create</button>
+        </form>
       </div>
     )
   }
